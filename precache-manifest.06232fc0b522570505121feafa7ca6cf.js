290725343bfe05f0bf6e0e943321dcf17f55f810
LiptuN <liptunred@gmail.com>
2020-09-03 20:15:50 +0200

    Init

---
+++ precache-manifest.06232fc0b522570505121feafa7ca6cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99f8e087a10b6af7eb0598d2f7476fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef0ce79dcda427272644",
+    "url": "/static/css/main.47dc0358.chunk.css"
+  },
+  {
+    "revision": "bb3086dd312f2f4c694b",
+    "url": "/static/js/2.debf300e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.debf300e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef0ce79dcda427272644",
+    "url": "/static/js/main.3e9379c3.chunk.js"
+  },
+  {
+    "revision": "f35db6047f5457f86eec",
+    "url": "/static/js/runtime-main.d2203a53.js"
+  },
+  {
+    "revision": "9e6e914cea414b7156adc02cafb0c42e",
+    "url": "/static/media/pepsi.9e6e914c.woff"
+  }
+]);
